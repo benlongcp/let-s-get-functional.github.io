661f1441a2f4bd11ecaaaf8d93fb437ff75febab
benlongcp <164564032+benlongcp@users.noreply.github.com>
2024-06-08 15:59:25 -0500

    up to friendsFirstLetterCount

--- projects/let-s-get-functional/index.js
+++ projects/let-s-get-functional/index.js
@@ -21,21 +21,205 @@ var _ = require('underbar');
  *    IMPORTANT: Make sure you replace <YOUR_GITHUB_FOLDER with your actual github folder name that is in your workspace.
  */
 
-var maleCount = function(array) {
+// var maleCount = function(array) {
+//     return _.filter(array, customer => customer.gender === 'male' ).length
+
+
+// _.filter = function(arr, func){
+//     //create an empty array to return later
+//     let retArr = [];
+//     //loop through the array
+//     for (let i = 0; i < arr.length; i++){
+//         //check if called input func for each array element is true
+//         if (func(arr[i], i, arr)){
+//             //push this array value to our retArr
+//             retArr.push(arr[i]);
+//         }
+
+//     }
+//     //returns retArr
+//     return retArr;
+// }
+
+
+let maleCount = function(array){
+    //set variable male customers to the invocation of filter, passing arguments of the input array and a function that checks if the 
+    let maleCusts = _.filter(array, function(customer){
+        return customer.gender === 'male'
+    });
+    return maleCusts.length;
+
+
+}
+    //     let males = _.reduce(array, function(accumulator, current){
+    //         if (current.gender === 'male'){
+    //             accumulator += 1;
+    //         }
+    //         return accumulator
+    //     }, 0);
+    //     return males;
+    // }
+
+
+    // //create return array
+    // let retArr = [];
+    // //iterate through the array
+    // for (let i = 0; i < array.length; i++){
+    //     if (array[i].gender === 'male'){
+    //         retArr.push(array[i]);
+    //     }
+    // }
+    // return retArr;
+    //}
+
+
+var femaleCount = function(array){
+    //set variable male customers to the invocation of filter, passing arguments of the input array and a function that checks if the 
+    let maleCusts = _.filter(array, function(customer){
+        return customer.gender === 'female'
+    });
+    return maleCusts.length;
+
 
 };
 
-var femaleCount;
 
-var oldestCustomer;
 
-var youngestCustomer;
 
-var averageBalance;
+var oldestCustomer = function(array){
+    //age tracker and an empty string to return later
+    let currentElderAge = 0;
+    let currentElder = ''
+    //loop through the array
+    for (let i = 0; i < array.length; i++){
+        //check if the currently iterated person is older or not
+        if (array[i].age > currentElderAge){
+            currentElderAge = array[i].age;
+            currentElder = array[i].name
+        }
+    }
+    return currentElder;
+}
 
-var firstLetterCount;
+// var oldestCustomer = function(array){
+//     let oldest = _.reduce(array, function(accumulator, current){
+//         if (current.age > accumulator.age){
+//             return current;
+//         } else {
+//             return accumulator;
 
-var friendFirstLetterCount;
+//         }
+//     })
+//     return oldest;
+// };
+
+
+
+var youngestCustomer = function(array){
+    //age tracker and an empty string to return later
+    let currentBabyAge = 99999999;
+    let currentBaby = ''
+    //loop through the array
+    for (let i = 0; i < array.length; i++){
+        //check if the currently iterated person is older or not
+        if (array[i].age < currentBabyAge){
+            currentBabyAge = array[i].age;
+            currentBaby = array[i].name
+        }
+    }
+    return currentBaby;
+}
+
+var averageBalance = array => {
+    let totBal;
+    let avgBal;
+    //loop through the array to access the balance properties
+    for (let i = 0; i < array.length; i++){
+
+        //set totBal to equal the itself plus current array's balance converted to a number
+        totBal += Number(array[i].balance.slice(1, array[i].length).replace(',',''));
+    }
+
+
+    return parseFloat((totBal / array.length).toFixed(3));
+};
+
+
+
+// ### 6: `firstLetterCount`
+//  - **Objective**: Find how many customer's names begin with a given letter
+//  - **Input**: `Array`, `Letter`
+//  - **Output**: `Number`
+//  - **Constraints**:
+
+
+//firstLetterCount is assigned to a function that takes parameters array and string
+var firstLetterCount = (array, letter) => {
+    //create a counter variable number to use and return later
+    let counter = 0;
+
+    //swap out our argument variable for a lowercased version to check against
+    let char = letter.toLowerCase()
+
+    //loop through the array
+    for (let i = 0; i < array.length; i++){
+
+        //check if the lowercased 0th index of the string contained in the current iterated array element's name property equals the input char
+        if (array[i].name[0].toLowerCase() === char){
+            
+            //increment counter
+            counter += 1;
+
+        }
+    }
+    //return counter
+    return counter;
+
+};
+
+
+// ### 7: `friendFirstLetterCount`
+//  - **Objective**: Find how many friends of a given customer have names that start with a given letter
+//  - **Input**: `Array`, `Customer`, `Letter`
+//  - **Output**: `Number`
+//  - **Constraints**:
+
+//assigned to a function that takes values of array, customer (presumably a string name of a customer), and a letter as parameters
+var friendFirstLetterCount = (array, customer, letter) => {
+    //create a counter variable for later use and return
+    let counter = 0;
+
+    //swap out the argument variable for for its lowercased version to check against
+    let char = letter.toLowerCase();
+
+    //loop through the array to look for customer
+    for (let i = 0; i < array.length; i++){
+        
+        //check if the current iterated array's object's name property is euqal to the input customer
+        if (array[i].name === customer){
+
+            //loop through the current iterated array-object's friends array
+            for (let subI = 0; subI < array[i].friends.length; subI++){
+
+                //check if the lowercased 0th index of the string contained in the current iterated array-object's current friends-array-object's name property equals the input char
+                if (array[i].friends[subI].name[0].toLowerCase() === char){
+
+                    //increment counter
+                    counter += 1;
+                }
+
+            }
+
+        }
+    }
+    //return counter
+    return counter;
+}
+
+
+
+
+
 
 var friendsCount;
 
